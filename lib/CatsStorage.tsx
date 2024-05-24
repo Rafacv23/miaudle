07d9a176ae93e3a508a7977782611bb9fee75ff3
Rafa Canosa <rafacv23@gmail.com>
2024-05-24 16:37:34 +0200

    new interface and methods

--- lib/CatsStorage.tsx
+++ lib/CatsStorage.tsx
@@ -1,9 +1,44 @@
 import create from "zustand"
-import type { CatStore } from "@/types"
+import stores from "@/lib/store.json"
+import upgrades from "@/lib/upgrades.json"
+import type { Store, Upgrade } from "@/types"
 
-export const useStore = create<CatStore>((set) => ({
+interface State {
+  playing: boolean
+  cats: number
+  upgrades: Upgrade[]
+  stores: Store[]
+  fetchUpgrades: () => void
+  fetchStores: () => void
+  increaseCats: () => void
+  removeAllCats: () => void
+  updateCats: (newCats: number) => void
+  setPlaying: (value: boolean) => void
+  reset: () => void
+}
+
+export const useStore = create<State>((set) => ({
+  playing: false,
   cats: 0,
-  increasePopulation: () => set((state) => ({ cats: state.cats + 1 })),
+  stores: [],
+  upgrades: [],
+  increaseCats: () => set((state) => ({ cats: state.cats + 1 })),
   removeAllCats: () => set({ cats: 0 }),
   updateCats: (newCats) => set({ cats: newCats }),
+
+  fetchStores: () => {
+    set({ stores })
+  },
+
+  fetchUpgrades: () => {
+    set({ upgrades })
+  },
+
+  setPlaying: (value: boolean) => {
+    set({ playing: value })
+  },
+
+  reset: () => {
+    set({ cats: 0, stores: [], upgrades: [] })
+  },
 }))
