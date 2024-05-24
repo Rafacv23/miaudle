8b74d650e6f0780b453edd975561f77ef24e5af0
Rafa Canosa <rafacv23@gmail.com>
2024-05-24 16:36:33 +0200

    logic to render play or start

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
+import { useStore } from "@/lib/CatsStorage"
 import Play from "../components/ui/Play"
+import Start from "@/components/ui/Start"
 
 export default function Home() {
+  let playing = useStore((state) => state.playing)
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-2xl text-center justify-center">
-        <Play />
+        {playing ? <Play /> : <Start />}
       </div>
     </section>
   )
