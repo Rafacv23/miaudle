c7f6d429cef58dfeabf249f9849510bb0a924d9f
Rafa Canosa <rafacv23@gmail.com>
2024-05-24 16:37:47 +0200

    fetching upgrades using global state

--- components/ui/Upgrades.tsx
+++ components/ui/Upgrades.tsx
@@ -1,5 +1,21 @@
+"use client"
+
 import React from "react"
+import { useStore } from "@/lib/CatsStorage"
 
 export default function Upgrades() {
-  return <div>Upgrades</div>
+  const fetchUpgrades = useStore((state) => state.fetchUpgrades)
+  const upgrades = useStore((state) => state.upgrades)
+
+  if (upgrades.length === 0) {
+    fetchUpgrades()
+  }
+  return (
+    <section>
+      <h2>Upgrades</h2>
+      <ul>
+        {upgrades ? upgrades.map((upgrade) => <li>{upgrade.name}</li>) : null}
+      </ul>
+    </section>
+  )
 }
