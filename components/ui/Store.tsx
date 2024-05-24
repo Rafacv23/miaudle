d57c10e057133969c7cd8136cfb8a3554bb683f4
Rafa Canosa <rafacv23@gmail.com>
2024-05-24 16:37:56 +0200

    fetching stores using global state

--- components/ui/Store.tsx
+++ components/ui/Store.tsx
@@ -1,5 +1,29 @@
+"use client"
+
 import React from "react"
+import { useStore } from "@/lib/CatsStorage"
 
 export default function Store() {
-  return <div>Store</div>
+  const fetchStores = useStore((state) => state.fetchStores)
+  const stores = useStore((state) => state.stores)
+
+  // Conditionally call fetchStores if stores array is empty
+  if (stores.length === 0) {
+    fetchStores()
+  }
+
+  return (
+    <section>
+      <h2>Store</h2>
+      <ul>
+        {stores.length > 0 ? (
+          stores.map((adquisition) => (
+            <li key={adquisition.name}>{adquisition.name}</li>
+          ))
+        ) : (
+          <li>Loading stores...</li>
+        )}
+      </ul>
+    </section>
+  )
 }
