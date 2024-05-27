84958bc633fbff7030a281018c7f59ca4c317a06
Rafa Canosa <rafacv23@gmail.com>
2024-05-27 12:58:54 +0200

    add logic to buy and store and refresh the component changed the price and isDisabled logic

--- components/ui/Store.tsx
+++ components/ui/Store.tsx
@@ -1,15 +1,39 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
 import { useStore } from "@/lib/CatsStorage"
+import { Button } from "@nextui-org/button"
 
 export default function Store() {
   const fetchStores = useStore((state) => state.fetchStores)
   const stores = useStore((state) => state.stores)
+  const cats = useStore((state) => state.cats)
+  const updateCats = useStore((state) => state.updateCats)
+  const catsPerSecond = useStore((state) => state.catsPerSecond)
+  const setCatsPerSecond = useStore((state) => state.setCatsPerSecond)
+  const setStores = useStore((state) => state.setStores)
 
   // Conditionally call fetchStores if stores array is empty
-  if (stores.length === 0) {
-    fetchStores()
+  useEffect(() => {
+    if (stores.length === 0) {
+      fetchStores()
+    }
+  }, [fetchStores, stores.length])
+
+  const buyStore = (storeId: number) => {
+    const store = stores.find((store) => store.id === storeId)
+    if (store && cats >= store.price) {
+      updateCats(cats - store.price)
+
+      setCatsPerSecond(catsPerSecond + store.catsPerSecond)
+
+      const updatedStores = stores.map((s) =>
+        s.id === storeId
+          ? { ...s, price: Math.ceil(s.price * s.multiplier) }
+          : s
+      )
+      setStores(updatedStores)
+    }
   }
 
   return (
@@ -19,9 +43,12 @@ export default function Store() {
         {stores.length > 0 ? (
           stores.map((adquisition) => (
             <li key={adquisition.id}>
-              <button>
+              <Button
+                isDisabled={cats < adquisition.price}
+                onClick={() => buyStore(adquisition.id)}
+              >
                 {adquisition.name} | {adquisition.price}
-              </button>
+              </Button>
             </li>
           ))
         ) : (
