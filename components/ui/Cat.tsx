e66f8893900fe681fb11c98cb1d87247ba8ffa6e
Rafa Canosa <rafacv23@gmail.com>
2024-05-27 12:58:02 +0200

    add autoIncrement function, alt to the img and new p

--- components/ui/Cat.tsx
+++ components/ui/Cat.tsx
@@ -4,10 +4,12 @@ import React from "react"
 import { Card, CardFooter } from "@nextui-org/card"
 import { Image } from "@nextui-org/image"
 import { Button } from "@nextui-org/button"
-import { useStore } from "@/lib/CatsStorage"
+import useAutoIncrementCats, { useStore } from "@/lib/CatsStorage"
 
 export default function Cat() {
+  useAutoIncrementCats()
   const cats = useStore((state) => state.cats)
+  const catsPerSecond = useStore((state) => state.catsPerSecond)
   const increaseCats = useStore((state) => state.increaseCats)
 
   return (
@@ -17,14 +19,16 @@ export default function Cat() {
         <button>About</button>
       </header>
       <Image
-        alt="Woman listening to music"
+        alt="Cat"
         className="object-cover"
         src="/cat.png"
         width={300}
         isZoomed
       />
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">Cats per second: {cats}</p>
+        <p className="text-tiny text-white/80">
+          Total cats: {cats} | Cats per second: {catsPerSecond}
+        </p>
         <Button
           className="text-tiny text-white bg-black/20"
           variant="flat"
