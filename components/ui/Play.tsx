cbba36bcd85f439e3b7d0efc6ceac78d06ade862
Rafa Canosa <rafacv23@gmail.com>
2024-06-06 15:31:19 +0200

    styles

--- components/ui/Play.tsx
+++ components/ui/Play.tsx
@@ -2,19 +2,17 @@ import React from "react"
 import Cat from "./Cat"
 import Store from "./Store"
 import Upgrades from "./Upgrades"
+import Header from "./Header"
 
 export default function Play() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center justify-center py-8 md:py-10">
-      <div className="md:col-span-1">
-        <Cat />
-      </div>
-      <div className="md:col-span-1">
-        <Store />
-      </div>
-      <div className="md:col-span-1">
-        <Upgrades />
-      </div>
+    <div
+      className="grid grid-cols-1 max-w-xl gap-4 items-
+    center justify-center py-8 md:py-10"
+    >
+      <Header />
+      <Cat />
+      <Store />
     </div>
   )
 }
