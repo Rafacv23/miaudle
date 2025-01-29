88b196e108590e3d4cd67b65a1f8eafff9cbf618
Rafa Canosa <rafacv23@gmail.com>
2025-01-29 13:44:36 +0100

    Enhance Passives component by changing passive name display to a header format

--- src/components/Passives.tsx
+++ src/components/Passives.tsx
@@ -6,7 +6,9 @@ export default function Passives() {
       <h2>Passives</h2>
       <ul>
         {getAllPassives().map((passive) => (
-          <li key={passive.name}>{passive.name}</li>
+          <li key={passive.name}>
+            <h2>{passive.name}</h2>
+          </li>
         ))}
       </ul>
     </div>
