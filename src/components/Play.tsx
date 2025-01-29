566037a3f3b4232471133062082638ad5d8cd2c0
Rafa Canosa <rafacv23@gmail.com>
2025-01-29 13:44:40 +0100

    Implement auto-increment of passive income and enhance Play component UI

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,8 +1,34 @@
+"use client"
+
+import { useMiaudleStore } from "@/lib/miaudleStore"
+import { useEffect } from "react"
+
 export default function Play() {
+  const {
+    totalCats,
+    clicks,
+    catsPerSecond,
+    incrementCats,
+    increasePassiveIncome,
+  } = useMiaudleStore()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      increasePassiveIncome()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [catsPerSecond, increasePassiveIncome])
+
   return (
     <div>
       <h1>Miaudle</h1>
-      <img src="cat.png" alt="cat" />
+      <button onClick={() => incrementCats(1)}>
+        <img src="cat.png" alt="cat" />
+      </button>
+      <h3>Total cats: {totalCats}</h3>
+      <h3>Cats per second: {catsPerSecond}</h3>
+      <p>Clicks totales: {clicks}</p>
     </div>
   )
 }
