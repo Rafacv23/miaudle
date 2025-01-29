62bdd864624ee3b06eb010ff02ffe9e09391c2f1
Rafa Canosa <rafacv23@gmail.com>
2025-01-29 14:35:11 +0100

    Refactor UpgradeCard to use InteractiveHoverButton for improved user interaction

--- src/components/UpgradeCard.tsx
+++ src/components/UpgradeCard.tsx
@@ -5,6 +5,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { useMiaudleStore } from "@/lib/miaudleStore"
+import { InteractiveHoverButton } from "./ui/interactive-hover-button"
 
 interface UpgradeCardProps {
   upgrade: Upgrade
@@ -22,33 +23,24 @@ export default function UpgradeCard({
   const canBuy = totalCats >= newCost
 
   return (
-    <div className="relative h-48 w-48">
-      <div className="absolute top-0 flex w-full justify-center">
-        <div
-          className={
-            canBuy
-              ? "left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-green-300 to-green-500 transition-all duration-1000"
-              : "left-0 h-[1px] animate-border-width rounded-full bg-gradient-to-r from-[rgba(17,17,17,0)] via-red-300 to-red-500 transition-all duration-1000"
-          }
-        />
-      </div>
-      <div className="flex h-full items-center justify-center rounded-md border border-gray-800 bg-gradient-to-b from-gray-950 to-black px-3 py-2">
-        <Tooltip key={upgrade.name}>
-          <TooltipTrigger>
-            <li
-              onClick={() => buyUpgrade(upgrade.id)}
-              className={canBuy ? "cursor-pointer" : "cursor-not-allowed"}
-            >
-              <h3>{upgrade.name}</h3>
-              <p>Times bought: {timesBought}</p>
-              <p>Cost: {newCost}</p>
-              <TooltipContent>
-                <p>{upgrade.description}</p>
-              </TooltipContent>
-            </li>
-          </TooltipTrigger>
-        </Tooltip>
-      </div>
-    </div>
+    <li key={upgrade.name}>
+      <Tooltip>
+        <TooltipTrigger>
+          <InteractiveHoverButton
+            canBuy={canBuy}
+            cost={newCost}
+            className={canBuy ? "cursor-pointer" : "cursor-not-allowed"}
+            onClick={() => buyUpgrade(upgrade.id)}
+          >
+            <h3>{upgrade.name}</h3>
+            <p>Times bought: {timesBought}</p>
+            <p>Cost: {newCost}</p>
+            <TooltipContent>
+              <p>{upgrade.description}</p>
+            </TooltipContent>
+          </InteractiveHoverButton>
+        </TooltipTrigger>
+      </Tooltip>
+    </li>
   )
 }
