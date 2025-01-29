d0c17740f03c84911b39cf0a1c82f6474de3454e
Rafa Canosa <rafacv23@gmail.com>
2025-01-29 12:28:17 +0100

    Refactor Home component layout to include Passives, Play, Upgrades, and Dock components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import Passives from "@/components/Passives"
+import Play from "@/components/Play"
+import { Dock } from "@/components/ui/dock"
+import Upgrades from "@/components/Upgrades"
+
 export default function Home() {
   return (
     <div className="grid grid-cols-6 grid-rows-5 gap-4">
-      <div className="col-span-2 row-span-4">1</div>
-      <div className="col-span-2 row-span-4 col-start-3">
-        <img src="cat.png" alt="cat" />
+      <div className="col-span-2 row-span-4">
+        <Passives />
       </div>
-      <div className="col-span-2 row-span-4 col-start-5">3</div>
-      <div className="col-span-6 row-start-5">4</div>
+      <div className="col-span-2 row-span-4 col-start-3">
+        <Play />
+      </div>
+      <div className="col-span-2 row-span-4 col-start-5">
+        <Upgrades />
+      </div>
+      <div className="col-span-6 row-start-5">
+        <Dock>esto es un dock</Dock>
+      </div>
     </div>
   )
 }
