fc7a2446a86acd5a36c1855bcedb96b2bb8a8476
Rafa Canosa <rafacv23@gmail.com>
2025-01-29 13:44:47 +0100

    Refactor Upgrades component to display unlocked upgrades with dynamic cost and purchase functionality

--- src/components/Upgrades.tsx
+++ src/components/Upgrades.tsx
@@ -1,14 +1,48 @@
+"use client"
+
+import { useMiaudleStore } from "@/lib/miaudleStore"
 import { getAllUpgrades } from "@/lib/upgrades"
+import { useEffect } from "react"
 
 export default function Upgrades() {
   const upgrades = getAllUpgrades()
+
+  const {
+    totalCats,
+    catsPerSecond,
+    clicks,
+    buyUpgrade,
+    checkUnlockedUpgrades,
+    unlockedUpgrades,
+    ownedUpgrades,
+  } = useMiaudleStore()
+
+  useEffect(() => {
+    checkUnlockedUpgrades()
+  }, [totalCats, catsPerSecond, clicks, checkUnlockedUpgrades])
+
   return (
     <div>
       <h2>Upgrades</h2>
       <ul>
-        {upgrades.map((upgrade) => (
-          <li key={upgrade.name}>{upgrade.name}</li>
-        ))}
+        {unlockedUpgrades.map((upgradeId) => {
+          const upgrade = upgrades.find((u) => u.id === upgradeId)
+          if (upgrade) {
+            const timesBought = ownedUpgrades[upgradeId] || 0
+            const newCost = Math.floor(
+              upgrade!.cost * Math.pow(1.15, timesBought)
+            )
+
+            return (
+              <li key={upgrade.name} onClick={() => buyUpgrade(upgrade.id)}>
+                <h3>{upgrade.name}</h3>
+                <p>Times bought: {timesBought}</p>
+                <p>Cost: {newCost}</p>
+                <button>Buy</button>
+              </li>
+            )
+          }
+        })}
       </ul>
     </div>
   )
