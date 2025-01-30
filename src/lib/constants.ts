2ccfe81c48ff36d48a3c5d1de9ddde46fae3976b
Rafa <rafacv23@gmail.com>
2025-01-30 13:35:12 +0100

    Add constants for external links in the application

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_REPO_URL = "https://github.com/rafacv23/miaudle"
+export const PORTFOLIO_URL = "https://www.rafacanosa.dev"
+export const LINKEDIN_URL = "https://ninjapath.vercel.app/linkedin"
