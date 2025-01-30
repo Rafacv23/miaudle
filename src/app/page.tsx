67643b0f24b769f200c684c8e7dc9cd33187bba5
Rafa <rafacv23@gmail.com>
2025-01-30 14:10:15 +0100

    Refactor Home component to remove Dock and associated links, and adjust grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,10 @@
 import Passives from "@/components/Passives"
 import Play from "@/components/Play"
-import { ThemeSwitch } from "@/components/theme/ThemeSwitch"
-import { buttonVariants } from "@/components/ui/button"
-import { Dock } from "@/components/ui/dock"
 import Upgrades from "@/components/Upgrades"
-import { GITHUB_REPO_URL, LINKEDIN_URL, PORTFOLIO_URL } from "@/lib/constants"
-import { Github, Info, Linkedin, User } from "lucide-react"
-import Link from "next/link"
 
 export default function Home() {
-  const dockData = [
-    {
-      name: "Github",
-      icon: Github,
-      url: GITHUB_REPO_URL,
-    },
-    {
-      name: "Linkedin",
-      icon: Linkedin,
-      url: LINKEDIN_URL,
-    },
-    {
-      name: "Rafa Canosa Portfolio",
-      icon: User,
-      url: PORTFOLIO_URL,
-    },
-    {
-      name: "About",
-      icon: Info,
-      url: "/about",
-    },
-  ]
-
   return (
-    <div className="grid grid-cols-6 grid-rows-5 gap-4">
+    <div className="grid grid-cols-6 grid-rows-4 gap-4">
       <div className="col-span-2 row-span-4">
         <Passives />
       </div>
@@ -42,21 +13,6 @@ export default function Home() {
       </div>
       <div className="col-span-2 row-span-4 col-start-5">
         <Upgrades />
-      </div>
-      <div className="col-span-6 row-start-5">
-        <Dock>
-          {dockData.map((item) => (
-            <Link
-              key={item.name}
-              href={item.url}
-              rel="noreferrer"
-              className={buttonVariants({ variant: "ghost", size: "icon" })}
-            >
-              <item.icon size={24} />
-            </Link>
-          ))}
-          <ThemeSwitch />
-        </Dock>
       </div>
     </div>
   )
