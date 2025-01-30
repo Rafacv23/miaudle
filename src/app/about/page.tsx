43624f842433164e1efc7e5130637ee452ff8128
Rafa <rafacv23@gmail.com>
2025-01-30 14:10:18 +0100

    Add About page component with basic layout and content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="grid place-content-center pt-20">
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  )
+}
